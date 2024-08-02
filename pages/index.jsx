6fba7e3cc9463bea139e8a716b2047d2346b33a9
Chatchakhet <chatchakhet.c@ku.th>
2024-08-02 14:51:43 +0700

    edit banner height

--- pages/index.jsx
+++ pages/index.jsx
@@ -43,7 +43,7 @@ export default function Home() {
         <div>
           <Image
             width="100%"
-            height="450px"
+            height="500px"
             src="assets/banner/maxresdefault 1.png"
           />
         </div>
